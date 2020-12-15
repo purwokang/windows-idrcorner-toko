f65f5e8485eef811dd924757566cd37ff08728f4
purwokang <purwo.com@gmail.com>
2020-12-15 14:35:41 +0700

    menambah file adddata di tokoAPI component. end of video CRUD #2 CREATE - Menambah data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './Header'
 import DaftarProduk from './DaftarProduk'
+import AddProduk from './AddProduk'
 
 function App() {
     return (
-        <div>
+        <BrowserRouter>
             <Header />
-            <DaftarProduk />
-        </div>
+            <Route path='/' exact component={DaftarProduk} />
+            <Route path='/add' component={AddProduk} />
+
+        </BrowserRouter>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-            <p className='navbar-brand'>Toko kPw</p>
+        <nav className='navbar navbar-dark bg-dark'>
+            <div className="container">
+                <p className='navbar-brand'>Toko kPw</p>
+
+                <ul className='nav nav-pills justify-content-end'>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-link'>Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/add' className='nav-link'>Add Produk</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
